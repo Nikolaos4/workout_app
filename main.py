7b79c3fa7f39823ddafabe1295ca02476d93f12c
Nick Anikanov <nickanikanov12@gmail.com>
2026-09-08 11:17:33 +0300

    fix main.py and README file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ count_task = int("3")
 task_done = 0
 
 
-def count_task(task_done):
+def Count_task(task_done):
     task1 = "Жим, 3 подхода по 8 раз, 80 кг"
     task_done += 1
     task2 = "Присед, 2 подхода по 10 раз, 100 кг"
@@ -17,7 +17,7 @@ def count_task(task_done):
 
 def trening_result(result):
     if result == True:
-        print(f"Тренировка выполнена! Количество сделанных упражнений = {count_task(task_done)}")
+        print(f"Тренировка выполнена! Количество сделанных упражнений = {Count_task(task_done)}")
     else:
         print("Ты лентяй!!!")
 
